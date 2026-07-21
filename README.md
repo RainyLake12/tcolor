2199b4cb33c48901689ba320b55c8b842f0972b7
RainyLake12 <knode455@gmail.com>
2026-07-21 22:52:18 +0300

    All the code

---
+++ README.md
@@ -0,0 +1,10 @@
+# Tcolor
+
+Easily change your terminal's text/background color without memorizing codes or magic numbers.
+Just type : ```tcolor <text|background> <color>```
+
+
+
+
+
+I use arch btw
---
+++ main.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <string.h>
+
+struct color
+{
+  char* name;
+  int escape_code_number;
+};
+
+struct color colors[16] =
+{
+  {"Black", 30},
+  {"Red", 31},
+  {"Green", 32},
+  {"Yellow", 33},
+  {"Blue", 34},
+  {"Magenta", 35},
+  {"Cyan", 36},
+  {"White", 37},
+  {"Lightblack", 90},
+  {"Lightred", 91},
+  {"Lightgreen", 92},
+  {"Lightyellow", 93},
+  {"Lightblue", 94},
+  {"Lightmagenta", 95},
+  {"Lightcyan", 96},
+  {"Lightwhite", 97}
+};
+
+void usage(char* name)
+{
+  printf("Usage\n");
+  printf("-----------------\n");
+  printf("%s <text|background> <color>\n"); 
+  for (int i = 0; i < sizeof(colors)/sizeof(struct color); i++)
+  {
+    printf("\x1b[%im%s\n", colors[i].escape_code_number, colors[i].name);
+  }
+
+  printf("\x1b[0m");
+}
+
+int main(int argc, char** argv)
+{
+  if (argc != 3)
+  {
+    printf("Too few arguments!\n");
+    usage(*argv);
+    return 1;
+  }
+
+  if (strcmp(argv[1], "text") == 0)
+  {
+    for (int i = 0; i < sizeof(colors)/sizeof(struct color); i++)
+    {
+      if (strcmp(argv[2], colors[i].name) == 0)
+      {
+        printf("\x1b[%im", colors[i].escape_code_number);
+        return 0;
+      }
+    }
+    printf("Invalid color!\n");
+    usage(*argv);
+    return 0;
+  } else if (strcmp(argv[1], "background") == 0)
+  {
+    for (int i = 0; i < sizeof(colors)/sizeof(struct color); i++)
+    {
+      if (strcmp(argv[2], colors[i].name) == 0)
+      {
+        printf("\x1b[%im", colors[i].escape_code_number + 60);
+        return 0;
+      }
+    }
+  printf("Invalid color!\n");
+    usage(*argv);
+    return 0;
+  } else
+  {
+    printf("Invalid first argument!\n");
+    usage(*argv);
+    return 1;
+  }
+}
+
